bcae387be1b30bf73a8350d9c2f855653ef2c49d
DominikTag <dominik.adamczewski@tagvenue.com>
2022-08-31 21:59:42 +0200

    Add 3 additional kyu7 katas

---
+++ loveVsFriendship.js
@@ -0,0 +1,29 @@
+/*
+If　a = 1, b = 2, c = 3 ... z = 26
+
+Then l + o + v + e = 54
+
+and f + r + i + e + n + d + s + h + i + p = 108
+
+So friendship is twice stronger than love :-)
+
+The input will always be in lowercase and never be empty.
+
+Create a function that returns the value of the word, based on its letters, as integer.
+*/
+
+function wordsToScore(string){
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let stringToArray = string.split('');
+    let indexes = [];
+    
+    stringToArray.forEach(element => {
+      indexes.push(alphabet.indexOf(element));
+    })
+    
+    const score = indexes.map(element => {
+      return element + 1;
+    })
+    
+    return score.reduce((a, b) => a + b, 0);
+}
